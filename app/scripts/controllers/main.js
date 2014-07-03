23446ce92dbb9eed02088bb88c469238f385cf38
Julian Poyourow <julianpoyo@gmail.com>
2014-07-03 12:58:39 -0700

    Removed unnecessary controller code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,42 +15,7 @@ angular.module('yoFrontApp')
       'Karma'
     ];
 
-    $scope.login = function(){
-
-        var uuid = Math.floor(Math.random() * 100000000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 1000000000000);
-        var iid = Math.floor(Math.random() * 100000000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 1000000000000);
-
-
-        var loginjson = { 
-            "v": "a1.4.1",
-            "username": $scope.userform.username,
-            "user_password": $scope.userform.password,
-            "uuid": uuid,
-            "iid": iid
-        }
-
-        var response = UserLogin.submit(loginjson, function(){
-
-            var uuid = Math.floor(Math.random() * 100000000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 1000000000000);
-
-            var updatejson = { 
-                "data": {
-                    "objectId": response.objectId,
-                    "didEnterPassword": true
-                },
-                "v": "a1.4.1",
-                "uuid": uuid,
-                "iid": iid,
-                "session_token": response.result.session_token,
-                "classname": "_User"
-            }
-
-            Update.get(updatejson);
-
-        });
-
-    }
-
+    
 
   });
 
