df032c4b0b8a1313f064b2faa19184cf1d435b09
Julian Poyourow <julianpoyo@gmail.com>
2014-07-04 03:29:49 -0700

    Added redirect after login form submit

--- app/scripts/controllers/loginregister.js
+++ app/scripts/controllers/loginregister.js
@@ -15,6 +15,11 @@ angular.module('yoFrontApp')
       'Karma'
     ];
 
+    $scope.go = function(requrl){
+        console.log("url switch for " + requrl);
+        window.location.href = requrl;
+    }
+
     $scope.login = function(){
 
         var iid = Math.floor(Math.random() * 100000000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 10000) + "-" + Math.floor(Math.random() * 1000000000000);
@@ -31,6 +36,8 @@ angular.module('yoFrontApp')
             document.cookie = "session_token=" + $scope.loginfinish.result.session_token + "; expires=Sun, 18 Jan 2037 12:00:00 GMT";
             document.cookie = "username=" + $scope.loginfinish.result.data.username + "; expires=Sun, 18 Jan 2037 12:00:00 GMT";
             document.cookie = "udid=" + $scope.loginfinish.result.data.udid + "; expires=Sun, 18 Jan 2037 12:00:00 GMT";
+
+            $scope.go("/#/");
 
         });
     }
